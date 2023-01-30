4d6cb1d7f097c19388ccb62d4772970f965d5901
Meow <50887230+itzTheMeow@users.noreply.github.com>
2023-01-30 18:00:36 -0500

    mobile navbar layout start

--- src/navbar/Navbar.svelte
+++ src/navbar/Navbar.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
-  import { IconCodePlus, IconLogin, IconPhotoUp } from "@tabler/icons-svelte";
+  import { IconCodePlus, IconLogin, IconMenu2, IconPhotoUp } from "@tabler/icons-svelte";
   import { MyAccount } from "Auth";
+  import { ScreenWidth } from "State";
   import NavbarButton from "./NavbarButton.svelte";
 </script>
 
 <div class="w-full h-16 flex items-center px-3 gap-2 bg-black bg-opacity-10">
-  <NavbarButton href="/">Home</NavbarButton>
-  <NavbarButton href="/">Blog</NavbarButton>
-  <NavbarButton href="/">Portfolio</NavbarButton>
+  {#if $ScreenWidth > 600}
+    <NavbarButton href="/">Home</NavbarButton>
+    <NavbarButton href="/">Blog</NavbarButton>
+    <NavbarButton href="/">Portfolio</NavbarButton>
+  {:else}
+    <NavbarButton href="/"><IconMenu2 /></NavbarButton>
+  {/if}
   <div class="flex items-center gap-2 ml-auto">
     <NavbarButton href="/" square="Upload Image"><IconPhotoUp /></NavbarButton>
     <NavbarButton href="/" square="Upload Code"><IconCodePlus /></NavbarButton>
