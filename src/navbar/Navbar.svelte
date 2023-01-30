72240f48ec5b123ae15f4cdd635a51abcf3a7e30
Meow <me@itsmeow.cat>
2023-01-30 03:25:08 -0500

    resize nav

--- src/navbar/Navbar.svelte
+++ src/navbar/Navbar.svelte
@@ -4,7 +4,7 @@
   import NavbarButton from "./NavbarButton.svelte";
 </script>
 
-<div class="w-full h-14 flex items-center px-4 py-3 gap-2" style:background={Colors.navbar}>
+<div class="w-full h-16 flex items-center px-3 gap-2" style:background={Colors.navbar}>
   <NavbarButton href="/">Home</NavbarButton>
   <NavbarButton href="/">Blog</NavbarButton>
   <NavbarButton href="/">Portfolio</NavbarButton>
