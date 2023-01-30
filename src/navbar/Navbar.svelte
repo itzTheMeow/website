1ab1c18d67993bde6c21a8d9aa12f1d9a9e65d08
Meow <me@itsmeow.cat>
2023-01-30 03:22:20 -0500

    add avatar

--- src/navbar/Navbar.svelte
+++ src/navbar/Navbar.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { IconCodePlus, IconPhotoUp } from "@tabler/icons-svelte";
-  import { Colors } from "Colors";
+  import { Colors, Gradient, Gradients } from "Colors";
   import NavbarButton from "./NavbarButton.svelte";
 </script>
 
@@ -11,5 +11,15 @@
   <div class="flex items-center gap-2 ml-auto">
     <NavbarButton href="/" square><IconPhotoUp /></NavbarButton>
     <NavbarButton href="/" square><IconCodePlus /></NavbarButton>
+    <div
+      class="w-10 h-10 rounded-full flex items-center justify-center"
+      style:background={Gradient(Gradients.Blue)}
+    >
+      <img
+        class="w-9 h-9 rounded-full"
+        src="https://s.gravatar.com/avatar/93ea38bcb080e3697a8bb93e480e7e93?s=80"
+        alt=""
+      />
+    </div>
   </div>
 </div>
