58ede9c687bb97d341d72cb7ebc41f020bd0af03
Meow <50887230+itzTheMeow@users.noreply.github.com>
2023-01-30 16:30:53 -0500

    add font and update animations

--- src/routes/splash/index.svelte
+++ src/routes/splash/index.svelte
@@ -9,17 +9,25 @@
   <div
     class="flex flex-col gap-0.5 {$ScreenWidth <= 800
       ? 'mx-auto mt-[7%]'
-      : 'ml-[15%] mt-[10%]'} w-fit"
+      : 'ml-[15%] mt-[10%]'} w-fit font-[Nevis]"
   >
     <h1
       class="{$ScreenWidth <= 425
         ? 'text-4xl'
-        : 'text-6xl'} font-bold bg-clip-text text-transparent font-[Nevis]"
+        : 'text-6xl'} font-bold bg-clip-text text-transparent"
       style:background-image={Gradient(Gradients.Orange)}
-      in:fly={{ duration: 1000, x: 0, y: -35 }}
+      in:fly={{ duration: 900, x: 0, y: -35 }}
     >
       Hi, I'm Meow
     </h1>
-    <p class="font-[Nevis]" in:blur={{ delay: 600, duration: 800 }}>I make stuff.</p>
+    <p in:blur={{ delay: 400, duration: 800 }}>I make stuff.</p>
+  </div>
+  <div class="ml-[10%] mt-8">
+    <h2
+      in:fly={{ duration: 300, x: -15, delay: 1200 }}
+      class="text-sm uppercase brightness-[.65] font-[Quicksand] font-semibold"
+    >
+      Check it Out
+    </h2>
   </div>
 </Page>
