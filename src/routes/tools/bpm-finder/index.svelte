45faf062c90f485e4a97339d056540ea6371dcfd
Meow <50887230+itzTheMeow@users.noreply.github.com>
2023-02-01 16:49:41 -0500

    more color

--- src/routes/tools/bpm-finder/index.svelte
+++ src/routes/tools/bpm-finder/index.svelte
@@ -6,7 +6,7 @@
   <h1 class="text-2xl font-semibold">BPM Finder</h1>
   <p class="text-sm max-w-md">
     Upload an audio file to detect its BPM. All detection is done in the browser, meaning whatever
-    media your browser supports will work. (try mp3, ogg, wav, or m4a)<br />
-    Please Note: This method is not 100% accurate.
+    media your browser supports will work. (try mp3, ogg, wav, or m4a)
   </p>
+  <p class="text-xs">Please Note: This method is not 100% accurate.</p>
 </Page>
