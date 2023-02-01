ba89214ef5e27111a585525a7af858eba4c6435e
Meow <50887230+itzTheMeow@users.noreply.github.com>
2023-02-01 17:31:41 -0500

    testing of uploader

---
+++ src/UI/FileInput.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { IconUpload } from "@tabler/icons-svelte";
+  import { Colors } from "Colors";
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  export let text = "Choose File";
+</script>
+
+<div>
+  <input type="file" class="hidden" />
+  <div class="btn gap-1 hover:blur-[1px]" style:background={Colors.tertiary}>
+    <IconUpload size={20} />
+    {text}
+  </div>
+</div>
--- src/routes/tools/bpm-finder/index.svelte
+++ src/routes/tools/bpm-finder/index.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import FileInput from "UI/FileInput.svelte";
   import Page from "UI/Page.svelte";
 </script>
 
@@ -9,4 +10,5 @@
     media your browser supports will work. (try mp3, ogg, wav, or m4a)
   </p>
   <p class="text-xs">Please Note: This method is not 100% accurate.</p>
+  <FileInput text="Upload Audio" />
 </Page>
