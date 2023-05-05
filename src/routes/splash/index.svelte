b6515e41ecc43e436804657bb6cfabc11690686d
Meow <me@itsmeow.cat>
2023-05-05 19:15:36 -0400

    finish projects

--- src/routes/splash/index.svelte
+++ src/routes/splash/index.svelte
@@ -30,22 +30,59 @@
     >
       Check it Out
     </h2>
+  </div>
+  <div class="flex flex-wrap px-4 gap-1 justify-start">
+    <Project offset={1} name="Trade Hub" icon="https://cdn.nvst.ly/static/logo.svg" color="#1dbac5">
+      Trade Hub is a trade tracking/sharing app.
+    </Project>
     <Project
+      offset={2}
       name="YTMusic"
       icon="https://raw.githubusercontent.com/itzTheMeow/YTMusic/main/logo.svg"
       color="#661ae6"
     >
-      test
-    </Project>
-    <Project name="Trade Hub" icon="https://cdn.nvst.ly/static/logo.svg" color="#1dbac5">
-      test
+      A music downloader dashboard.
     </Project>
     <Project
+      offset={3}
       name="RevKit"
       icon="https://raw.githubusercontent.com/Revolt-Unofficial-Clients/revkit/master/revkit-logo.svg"
       color="#ff4654"
     >
-      test
+      A client library for Revolt.
+    </Project>
+    <Project
+      offset={4}
+      name="PHLASH"
+      icon="https://raw.githubusercontent.com/itzTheMeow/revolt-phlash/master/PHLASH.png"
+      color="#5b00c3"
+    >
+      A Revolt bot made using RevKit.
+    </Project>
+    <Project
+      offset={5}
+      name="Svolte"
+      icon="https://raw.githubusercontent.com/itzTheMeow/revolt-svolte/master/svolte-logo.svg"
+      color="#ff4654"
+    >
+      A Revolt client using Revkit.
+    </Project>
+    <Project
+      offset={6}
+      name="Revolt Subsystem for Discord"
+      icon="https://raw.githubusercontent.com/itzTheMeow/revolt-subsystem-discord/master/rsd-logo-dark.svg"
+      color="#ff4654"
+    >
+      A bridge providing 1-1 translation from Revolt's API to Discord's. Allows you to connect to a
+      Discord account using a Revolt client.
+    </Project>
+    <Project
+      offset={7}
+      name="NinjaIOUtils"
+      icon="https://raw.githubusercontent.com/itzTheMeow/NinjaIOUtils/master/logo.png"
+      color="#344152"
+    >
+      A utility script and modloader for the browser game <a href="https://ninja.io">ninja.io</a>.
     </Project>
   </div>
 </Page>
--- src/routes/splash/project.svelte
+++ src/routes/splash/project.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
   import { Colors } from "Colors";
+  import { blur } from "svelte/transition";
   import { generateUltrablurCSS } from "ultrablur";
 
-  export let name: string, icon: string, color: string;
+  const BASE_DELAY = 1200;
+
+  export let offset: number, name: string, icon: string, color: string;
 </script>
 
 <div
-  class="rounded-lg w-1/4 p-3 box-border"
+  class="rounded-lg w-[calc(33%-0.25rem)] p-3 box-border"
   style:background={generateUltrablurCSS(color, Colors.primary, 25, 4)}
+  in:blur={{ delay: BASE_DELAY + 150 * offset, duration: 800 }}
 >
   <div class="flex rounded items-center gap-2 w-fit p-2" style:background={Colors.secondary}>
     <img class="w-10 h-10 object-contain" src={icon} alt={name} crossorigin="anonymous" />
